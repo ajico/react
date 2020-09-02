413a6af311f0daf2e0912d3f9f74cd184c028604
aji prayitno <aji@smkn8semarang.sch.id>
2020-09-02 12:57:14 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Register from './Register';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './Home';
+
+function App() {
+  return (
+
+    <Router>
+     <Switch>
+       <Route exact path='/'>
+         <Home/>
+       </Route>
+       <Route path='/register'>
+         <Register/>
+       </Route>
+     </Switch>
+   </Router>
+
+  );
+}
+
+export default App;
